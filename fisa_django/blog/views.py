6e7445d94c78d1af22bb93e41b475009bddeebcd
thisispaperdoll <paperdoll0316@gmail.com>
2024-08-27 16:25:11 +0900

    change views.py

--- fisa_django/blog/views.py
+++ fisa_django/blog/views.py
@@ -1,3 +1,5 @@
+from django.forms import BaseModelForm
+from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Post
 from django.views.generic import ListView, DetailView, CreateView
@@ -5,8 +7,41 @@ from django.views.generic import ListView, DetailView, CreateView
 
 class PostCreate(CreateView):
     model = Post 
-    fields = ["title", "content", "head_image", "tag"]
+    fields = ["title", "content", "head_image"]
 
+    # CreateView가 내장한 함수 - 오버라이딩 
+    # tag는 참조관계이므로 Tag 테이블에 등록된 태그만 쓸 수 있는 상황
+    # 임의로 방문자가 form에 Tag를 달아서 보내도록 form_valid()에 결과를 임시로 담아두고
+    # 저장된 포스트로 돌아오도록 
+    # def form_valid(self, form):
+    #     current_user = self.request.user
+    #     if current_user.is_authenticated and (current_user.is_staff or current_user.is_superuser):
+    #         form.instance.author = current_user
+    #         response = super(PostCreate, self).form_valid(form)
+
+    #         tags_str = self.request.POST.get('tags_str')
+    #         if tags_str:
+    #             tags_str = tags_str.strip()   # 책, 독후감, 작가명
+
+    #             tags_str = tags_str.replace(',', ';')
+    #             tags_list = tags_str.split(';')
+
+    #             for t in tags_list:
+    #                 t = t.strip()
+    #                 tag, is_tag_created = Tag.objects.get_or_create(tag_name=t)
+    #                 if is_tag_created:
+    #                     tag.slug = slugify(t, allow_unicode=True)
+    #                     tag.save()
+    #                 self.object.tag.add(tag)
+
+    #         return response
+
+    #     else:
+    #             return redirect('/blog/')
+
+
+        return super().form_valid(form)
+    
 class PostDetail(DetailView):
     model = Post 
     
