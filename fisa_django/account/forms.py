e954a14d356152883452e260fe36cf99eb36139d
thisispaperdoll <paperdoll0316@gmail.com>
2024-08-29 11:16:46 +0900

    with absolute urls

--- fisa_django/account/forms.py
+++ fisa_django/account/forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 class LoginForm(forms.Form):
     username = forms.CharField()
-    password = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)  # text를 받되 ****처리
